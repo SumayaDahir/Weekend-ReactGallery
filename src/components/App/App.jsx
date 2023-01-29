d57280b936d0f7dc2c201e32c468b9b2fd2b5a60
Maya <DahirowSumaia@gmail.com>
2023-01-29 15:33:48 -0600

    created a FetchData function and added an axios Get request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,39 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import GalleryItem from "./GalleryItem/GalleryItem";
 import GalleryList from "./GalleryList/GalleryList";
 import './App.css';
 
+function fetchData(setGalleryList) {
+  axios
+    .get("/gallery")
+    .then((response) => {
+      console.log(response.data)
+      setGalleryList(response.data);
+    })
+    .catch((err) => {
+      console.error("There was an error getting photos", err);
+    });
+}
+
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <GalleryList/>
-        <GalleryItem/>
-      </div>
-    );
+  const [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    fetchData(setGalleryList);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <section>
+        <GalleryList galleryList={galleryList} className="list" />
+      </section>
+    </div>
+  );
 }
 
 export default App;
+
 
