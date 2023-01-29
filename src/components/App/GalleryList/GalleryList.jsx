00ca4ef2da7c41af94fb43f44222ce592cbab5c1
Maya <DahirowSumaia@gmail.com>
2023-01-29 16:09:41 -0600

    added comments to GalleryList

--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -1,8 +1,11 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
 
+//created a GalleryList component that takes two props galleryList and className
 function GalleryList({ galleryList, className }) {
     console.log(galleryList);
     return (
+        // Using the map function to map through the gallerylist prop
+        // assigned a unique id to the key in the GalleryItem component with a classname and item
         <div className={className}>
             {galleryList.map((item) => (
                 <GalleryItem key={item.id} className="list-items" item={item} />
@@ -11,5 +14,5 @@ function GalleryList({ galleryList, className }) {
     );
   }
   
-  
+ //exporting the GalleryList component 
   export default GalleryList;
