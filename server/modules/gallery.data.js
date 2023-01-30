49988b7902e037a6f09074d1d832932c4da04d38
Maya <DahirowSumaia@gmail.com>
2023-01-29 20:27:55 -0600

    added styling to images and description

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/bali.jpg', description: 'Bali', likes: 0 }
+    { id: 1, path: 'images/turkey.jpg', description: 'Turkey', likes: 0 },
+    { id: 2, path: 'images/bali.jpg', description: 'Bali', likes: 0 },
+    { id: 3, path: 'images/tulum.jpg', description: 'Tulum', likes: 0 },
+    { id: 4, path: 'images/kenya.jpg', description: 'Kenya', likes: 0 },
+    { id: 5, path: 'images/dubai.jpg', description: 'Dubai', likes: 0 },
+    { id: 6, path: 'images/italy.jpg', description: 'Italy', likes: 0 },
 
 ];
 
--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import './GalleryItem.css';
 
 function GalleryItem({ item, className }) {
   const [showDescription, setShowDescription] = useState(false);
@@ -7,18 +8,24 @@ function GalleryItem({ item, className }) {
 
   return (
     <div className={className}>
+      
       <div style={{ display: showDescription ? "none" : "block" }}>
+      <div className="images-container">
         <img
           src={item.path}
           onClick={() => setShowDescription(!showDescription)}
         />
+        </div>
       </div>
       <div style={{ display: showDescription ? "block" : "none" }}>
-        <p>{item.description}</p>
+      <p className="description">{item.description}</p>
       </div>
-      <button onClick={() => setLikes(likes + 1)}>LOVE!</button>
+      <div className="likes-container">
+      <button onClick={() => setLikes(likes + 1)}>LOVE</button>
       <p> {likes} People love this</p>
+      </div>
     </div>
+    
   );
 }
 
