bf723491a605c9a0aa9f8bddaca3a63a3c56d401
Maya <DahirowSumaia@gmail.com>
2023-01-29 18:46:25 -0600

    made changes to galleryitem component

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,13 +1,25 @@
-function GalleryItem() {
-    return (
+import React from "react";
+import { useState } from "react";
 
-        <>
-        <img src="images/goat_small.jpg"/>
-        <img src="images/goat_small.jpg"/>
-        <img src="images/goat_small.jpg"/>
+function GalleryItem({ item, className }) {
+  const [showDescription, setShowDescription] = useState(false);
+  const [likes, setLikes] = useState(0);
 
-        </>
-    )
+  return (
+    <div className={className}>
+      <div style={{ display: showDescription ? "none" : "invisable" }}>
+        <img
+          src={item.path}
+          onClick={() => setShowDescription(!showDescription)}
+        />
+      </div>
+      <div style={{ display: showDescription ? "invisable" : "none" }}>
+        <p>{item.description}</p>
+      </div>
+      <button onClick={() => setLikes(likes + 1)}>LOVE!</button>
+      <p> {likes} People love this</p>
+    </div>
+  );
 }
 
 export default GalleryItem;
